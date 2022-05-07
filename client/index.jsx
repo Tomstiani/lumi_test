b84069b56baa32fb05989bb0c45fe47a331747e6
Tom Stian <toms.ingebretson@gmail.com>
2022-05-07 16:11:25 +0200

    users now displays on page

--- client/index.jsx
+++ client/index.jsx
@@ -1,8 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 
 const Application = () => {
-  return <div>index</div>;
+  return (
+    <div>
+      <h1>Brukere</h1>
+      <UserList />
+    </div>
+  );
+};
+
+const UserList = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    try {
+      fetch("/api/users")
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          setUsers(data);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+    setLoading(false);
+  }, [users]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div>
+      <ul>
+        {users.map((user) => (
+          <li key={user.phone}>{user.first_name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 ReactDOM.render(<Application />, document.getElementById("root"));
--- server/usersApi.js
+++ server/usersApi.js
@@ -5,7 +5,12 @@ export default function usersApi() {
   const router = Router();
 
   router.get("/", (req, res) => {
-    res.send("Users sent!");
+    fs.readFile("./output.json", "utf8", (err, data) => {
+      if (err) {
+        res.status(500).send(err);
+      }
+      res.send(JSON.parse(data));
+    });
   });
 
   router.post("/", (req, res) => {
