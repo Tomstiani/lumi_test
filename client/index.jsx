8860b39b481728bc30e241172ecec6002b6e4ba4
Tom Stian <toms.ingebretson@gmail.com>
2022-05-07 15:43:23 +0200

    Created express/react app with parcel

---
+++ client/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Application = () => {
+  return <div>index</div>;
+};
+
+ReactDOM.render(<Application />, document.getElementById("root"));
---
+++ server/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import * as path from "path";
+
+const app = express();
+
+app.use(express.static("../client/dist/"));
+
+app.use((req, res, next) => {
+  if (req.method === "GET" && !req.path.startsWith("/api")) {
+    res.sendFile(path.resolve("../client/dist/index.html"));
+  } else {
+    next();
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(
+    `Server is running! Listening on http://localhost:${
+      process.env.PORT || 3000
+    }`
+  );
+});
